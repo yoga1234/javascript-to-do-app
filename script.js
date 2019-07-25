ce097b0d6b4c771bec7078a256d5ded7b9612376
Yogasmara <septianayoga30@gmail.com>
2019-07-25 15:21:39 +0700

    add toggle key

--- script.js
+++ script.js
@@ -13,17 +13,40 @@ function addTodo(text){
   //inserting data into todoItems array
   todoItems.push(todo);
   // print into console
+
+  // selecting the js-todo-list class into const list
   const list = document.querySelector('.js-todo-list');
+  // insertAdjacentHTML using for parses the specified text as HTML or XML
+  //insertAdjacentHTML(position, text);
   list.insertAdjacentHTML('beforeend',`
     <li class="todo-item" data-key="${todo.id}">
       <input id="${todo.id}" type="checkbox"/>
       <label for="${todo.id}" class="tick js-tick"></label>
       <span>${todo.text}</span>
       <button class="delete-todo js-delete-todo">
-
+        &#10006;
       </button>
     </li>
   `);
+
+  list.addEventListener('click', event => {
+    if (event.target.classList.contains('js-tick')) {
+      const itemKey = event.target.parentElement.dataset.key;
+      toggleDone(itemKey);
+    }
+  });
+
+  function toggleDone(key){
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    todoItems[index].checked = !todoItems[index].checked;
+
+    const item = document.querySelector(`[data-key='${key}']`);
+    if(todoItems[index].checked){
+      item.classList.add('done');
+    } else {
+      item.classList.remove('done');
+    }
+  }
 }
 
 // selecting a form named js-form
